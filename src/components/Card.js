09d1133b78dd87a3c875327c44a49beab7bbe448
Sam <samfelt@users.noreply.github.com>
2023-01-28 16:39:06 -0800

    first commit

---
+++ src/components/Card.js
@@ -0,0 +1,32 @@
+import { useState } from 'react'
+
+export default function Card() {
+
+
+    const [cardOpen, setCardOpen] = useState(false);
+
+    const handleClick = () => {
+        console.log("Handing Click")
+        setCardOpen(prev => !prev)
+    }
+
+    return(
+      <div className={cardOpen ? "card open" : "card"} onClick={handleClick}>
+
+        <div className="left-card">
+          <div className="card-front">
+            <img src="/img/cover.jpg" alt="christmas card cover" />
+          </div>
+          <div className="card-back">
+          </div>
+        </div>
+
+        <div className="right-card">
+          <p>You are giving to</p>
+          <h2>Tracy</h2> 
+          <p></p>
+          <p>good luck...</p>
+        </div>
+      </div>
+    )
+}
