8d20a22902cd67f324bbb0905a9c147694bb88da
Sam <samfelt@users.noreply.github.com>
2023-01-29 18:28:03 -0800

    Update card transition and content

--- src/components/Card.js
+++ src/components/Card.js
@@ -12,21 +12,30 @@ export default function Card() {
     }
 
     return(
-      <div className={cardOpen ? "card open" : "card"} onClick={handleClick}>
+        <div className={`card ${cardOpen ? "open" : ""}`} onClick={handleClick}>
 
-        <div className="left-card">
+        <div className="top-card">
           <div className="card-front">
             <img src="/img/cover.jpg" alt="christmas card cover" />
           </div>
           <div className="card-back">
+            <br/>
+            <br/>
+            <p>You are giving to</p>
+            <h2>Tracy</h2> 
+            <p></p>
+            <p>good luck...</p>
           </div>
         </div>
 
-        <div className="right-card">
-          <p>You are giving to</p>
-          <h2>Tracy</h2> 
-          <p></p>
-          <p>good luck...</p>
+        <div className="bottom-card">
+          <h2>What They Want for Christmas</h2> 
+          <ul>
+            <li>Delicious Chips</li>
+            <li>Gift Cards</li>
+            <li>Some peace and quite</li>
+            <li>More wine, always more wine</li>
+          </ul>
         </div>
       </div>
     )
